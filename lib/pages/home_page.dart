23afe4040f829f261a66d418f81f4b82aa50bccd
Louis FRADIN <louis.fradin@gmail.com>
2020-07-27 08:11:19 +0200

    home_page: add title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:Envely/blocs/authentication/authentication.dart';
 import 'package:Envely/models/models.dart';
+
+const TabNb = 5;
+const MajorTab = 3;
+
+enum EnvelyTabPosition {
+  BudgetTab,
+  SpendingTab,
+  AssistantTab,
+  AccountsTab,
+  ParametersTab
+}
 
 class HomePage extends StatefulWidget {
   final User user;
@@ -13,35 +25,149 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController =
+        new TabController(vsync: this, length: TabNb, initialIndex: MajorTab)
+          ..addListener(() {
+            // Set state to make sure that the [EnvelyTab] widgets get updated when changing tabs.
+            setState(() {});
+          });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: PreferredSize(
-              preferredSize: Size.fromHeight(kToolbarHeight),
-              child: Container(
-                  color: Theme.of(context).primaryColor,
-                  child: SafeArea(
-                      child: TabBar(
-                          indicatorColor:
-                              Theme.of(context).secondaryHeaderColor,
-                          tabs: [
-                        Tab(icon: Icon(Icons.assistant)),
-                        Tab(icon: Icon(Icons.pie_chart)),
-                        Tab(icon: Icon(Icons.account_balance_wallet)),
-                        Tab(icon: Icon(Icons.account_circle)),
-                      ])))),
-          body: TabBarView(
-            children: [
-              Icon(Icons.assistant),
-              Icon(Icons.pie_chart),
-              Icon(Icons.account_balance_wallet),
-              Account(user: widget.user),
-            ],
-          ),
-        ));
+    ScreenUtil.init(context);
+    return Scaffold(
+      appBar: AppBar(
+          title: EnvelyTitle(
+        tabController: _tabController,
+      )),
+      bottomNavigationBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+              color: Theme.of(context).primaryColor,
+              child: SafeArea(
+                  child: TabBar(
+                      controller: _tabController,
+                      indicatorColor: Theme.of(context).secondaryHeaderColor,
+                      tabs: _buildTabs(context))))),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Icon(Icons.pie_chart),
+          Icon(Icons.assessment),
+          Icon(Icons.assistant),
+          Icon(Icons.account_balance_wallet),
+          Account(user: widget.user),
+        ],
+      ),
+      // floatingActionButton: Container(
+      //     margin: new EdgeInsets.all(ScreenUtil().setWidth(50)),
+      //     child: FloatingActionButton(
+      //       onPressed: () {
+      //         // Add your onPressed code here!
+      //       },
+      //       child: Icon(Icons.attach_money),
+      //       backgroundColor: Theme.of(context).primaryColor,
+      //     )),
+    );
+  }
+
+  List<Widget> _buildTabs(BuildContext context) {
+    Map<int, Widget> map = {};
+    List<Widget> list = [];
+
+    map[EnvelyTabPosition.BudgetTab.index] = EnvelyTab(
+        position: EnvelyTabPosition.BudgetTab.index,
+        text: "Budget",
+        iconData: Icons.pie_chart,
+        tabController: _tabController);
+    map[EnvelyTabPosition.SpendingTab.index] = EnvelyTab(
+        position: EnvelyTabPosition.SpendingTab.index,
+        text: "Spending",
+        iconData: Icons.assessment,
+        tabController: _tabController);
+    map[EnvelyTabPosition.AssistantTab.index] = EnvelyTab(
+        position: EnvelyTabPosition.AssistantTab.index,
+        text: "Assistant",
+        iconData: Icons.assistant,
+        tabController: _tabController);
+    map[EnvelyTabPosition.AccountsTab.index] = EnvelyTab(
+        position: EnvelyTabPosition.AccountsTab.index,
+        text: "Accounts",
+        iconData: Icons.account_balance_wallet,
+        tabController: _tabController);
+    map[EnvelyTabPosition.ParametersTab.index] = EnvelyTab(
+        position: EnvelyTabPosition.ParametersTab.index,
+        text: "Parameters",
+        iconData: Icons.account_circle,
+        tabController: _tabController);
+
+    for (int i = 0; i < TabNb; i++) list.add(map[i]);
+    return list;
+  }
+}
+
+class EnvelyTitle extends StatefulWidget {
+  final TabController tabController;
+
+  const EnvelyTitle({this.tabController});
+
+  @override
+  _EnvelyTitleState createState() => _EnvelyTitleState();
+}
+
+class _EnvelyTitleState extends State<EnvelyTitle> {
+  @override
+  Widget build(BuildContext context) {
+    if (widget.tabController.index == EnvelyTabPosition.BudgetTab.index)
+      return Text("Budget");
+    if (widget.tabController.index == EnvelyTabPosition.SpendingTab.index)
+      return Text("Spending");
+    if (widget.tabController.index == EnvelyTabPosition.AssistantTab.index)
+      return Text("Assistant");
+    if (widget.tabController.index == EnvelyTabPosition.AccountsTab.index)
+      return Text("Accounts");
+    if (widget.tabController.index == EnvelyTabPosition.ParametersTab.index)
+      return Text("Parameters");
+    return Text("unkown");
+  }
+}
+
+class EnvelyTab extends StatefulWidget {
+  final int position;
+  final TabController tabController;
+  final String text;
+  final IconData iconData;
+
+  const EnvelyTab(
+      {this.position, this.tabController, this.text, this.iconData});
+
+  @override
+  _EnvelyTabState createState() => _EnvelyTabState();
+}
+
+class _EnvelyTabState extends State<EnvelyTab> {
+  @override
+  Widget build(BuildContext context) {
+    String displayedText;
+
+    if (widget.tabController.index == widget.position)
+      displayedText = widget.text;
+
+    return Tab(text: displayedText, icon: Icon(widget.iconData));
   }
 }
 
@@ -79,41 +205,3 @@ class _AccountState extends State<Account> {
     );
   }
 }
-
-// class HomePage extends StatelessWidget {
-//   final User user;
-
-//   const HomePage({Key key, this.user}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         minimum: const EdgeInsets.all(16),
-//         child: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               Text(
-//                 'Welcome, ${user.name}',
-//                 style: TextStyle(fontSize: 24),
-//               ),
-//               const SizedBox(
-//                 height: 12,
-//               ),
-//               FlatButton(
-//                 textColor: Theme.of(context).primaryColor,
-//                 child: Text('Logout'),
-//                 onPressed: () {
-//                   // Add UserLoggedOut to authentication event stream.
-//                   BlocProvider.of<AuthenticationBloc>(context)
-//                       .add(UserLoggedOut());
-//                 },
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
